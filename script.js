00e9fa91f21e82d379b4cf218004936349559346
unknown <izazahmed0177@gmail.com>
2022-09-14 23:01:21 +0600

    time  problem complite

--- script.js
+++ script.js
@@ -80,7 +80,13 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+
+  //
+  const timeTakenRow = (finishTime - startTime) / 1000;
+  ///////
+  const timeTaken = parseInt(timeTakenRow);
+  ////
+
 
   // show result modal
   resultModal.innerHTML = "";
@@ -167,7 +173,14 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  //
+  const timeSpentRow = (currentTime - startTime) / 1000;
+  ////
+  const timeSpent = parseInt(timeSpentRow)
+  ///
+
+  // console.log(timeSpent)
+  // console.log(parseInt(timeSpent));
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
