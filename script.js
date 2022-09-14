6a47c03693bd3a4d469099cce2df622c591664d4
unknown <izazahmed0177@gmail.com>
2022-09-14 21:57:02 +0600

    starte button  complite

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 ///
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+// const startBtn = document.getElementById("start");
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -122,20 +123,31 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+
+    // countdownOverlay.innerHTML = '<h1>${count}</h1>';
+
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+
+      // countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
+
       display.classList.remove("inactive");
+      // display.classList.add("active");
+
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
+
     count--;
   }, 1000);
+
+
 };
 
 
